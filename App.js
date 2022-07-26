5c1754484e84277a49baa34db4431ec026c8fad2
shrutidhungana <shrutidhungana@prixa.net>
2022-07-26 12:55:26 +0545

    First Commit

--- App.js
+++ App.js
@@ -4,17 +4,50 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style= {styles.flex1}>
+        <Text>
+          1
+        </Text>
+      </View>
+      <View style = {styles.flex2}>
+        <Text>
+          2
+        </Text>
+      </View>
+      <View style = {styles.flex3}>
+        <Text>
+          3
+        </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    padding: 50,
+   flexDirection: 'row',
+    width: '80%',
+    height: 300,
+    justifyContent: 'space-around',
+    alignItems: 'stretch'
+  },
+  flex1: {
+    backgroundColor: 'red',
+     flex: 1  ,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  flex2: {
+    backgroundColor: 'blue',
+     flex: 2,
+    alignItems: 'center',
+    justifyContent:'center'
+  },
+  flex3: {
+    backgroundColor: 'green',
+    // flex: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
